911bb7765229e913f280cdc0697048a3953eda54
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-08 02:10:30 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,45 +7,58 @@ int main() {
 
   double total = 0;
 
-  Hotel hotel;
+  Hotel hotel; // Se crea un objeto de la clase Hotel. Este objeto administra todas las habitaciones
 
-  hotel.crea_ejemplos();
+  hotel.crea_ejemplos(); // Se cargan habitaciones de prueba.
 
   cout << "Habitaciones registradas:\n\n";
+
   hotel.muestra_habitaciones();
 
   cout << "\nHabitaciones individuales:\n\n";
+
   hotel.muestra_habitaciones("individual");
 
   cout << "\nHabitaciones dobles:\n\n";
+
   hotel.muestra_habitaciones("doble");
 
   cout << "\nSuites:\n\n";
+
   hotel.muestra_habitaciones("suite");
 
   total = hotel.calc_ingreso_total();
+
   cout << "\nIngreso total del hotel: " << total << endl;
 
   total = hotel.calc_ingreso_total("individual");
+
   cout << "Ingreso total por habitaciones individuales: " << total << endl;
 
   total = hotel.calc_ingreso_total("doble");
+
   cout << "Ingreso total por habitaciones dobles: " << total << endl;
 
   total = hotel.calc_ingreso_total("suite");
-  cout << "Ingreso total por suites: " << total << endl;
 
+  cout << "Ingreso total por suites: " << total << endl;
+  
   cout << "\nAgregando nuevas habitaciones...\n\n";
 
   hotel.agrega_individual(103, 3, 800, true);
-  hotel.agrega_doble(203, 2, 1300, 250);
-  hotel.agrega_suite(303, 4, 3000, 1200, 500);
 
+  hotel.agrega_doble(203, 2, 1300, 250);
+
+  hotel.agrega_suite(303, 4, 3000, 1200, 500);
+  
   cout << "Habitaciones actualizadas:\n\n";
+
   hotel.muestra_habitaciones();
 
   total = hotel.calc_ingreso_total();
+  
   cout << "\nNuevo ingreso total del hotel: " << total << endl;
 
   return 0;
+
 }
