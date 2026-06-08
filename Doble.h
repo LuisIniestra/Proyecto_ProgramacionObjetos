998febf08cb002390075064d5f6ba104ed142931
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-08 01:31:40 -0600

    Update Doble.h

--- Doble.h
+++ Doble.h
@@ -6,18 +6,27 @@
 
 using namespace std;
 
-class Doble: public Habitacion {
+class Doble: public Habitacion { // La clase Doble hereda de Habitacion.
 
   private:
     double cargo_extra;
+    // Atributo propio de la habitación doble
 
   public:
     Doble();
+    // Constructor por default
+
     Doble(int num, int noch, double precio);
+    // Constructor con parámetros basicos
+
     Doble(int num, int noch, double precio, double cargo);
+    // Constructor sobrecargado. Recibe numero, noches, precio y cargo extra (overloading)
 
     double costo_total() const override;
+    // Sobreescritura del método costo_total() de la clase abstracta
+
     string to_string() const override;
+    // Sobreescritura del método to_string() de la clase abstracta
 };
 
 #endif
