91c9ed2f457f1cddf1cc8e7101e29b9b50467f64
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-07 22:18:27 -0600

    Update habitacion.cpp

--- habitacion.cpp
+++ habitacion.cpp
@@ -1,4 +1,4 @@
-#include "Habitacion.h"
+#include "habitacion.h"
 #include <sstream>
 
 using namespace std;
@@ -17,35 +17,26 @@ Habitacion::Habitacion(int num, int noch, double precio, string tip) {
   tipo = tip;
 }
 
-int Habitacion::get_numero() {
+Habitacion::~Habitacion() {
+}
+
+int Habitacion::get_numero() const {
   return numero;
 }
 
-int Habitacion::get_noches() {
+int Habitacion::get_noches() const {
   return noches;
 }
 
-double Habitacion::get_precio_noche() {
+double Habitacion::get_precio_noche() const {
   return precio_noche;
 }
 
-string Habitacion::get_tipo() {
+string Habitacion::get_tipo() const {
   return tipo;
 }
 
-double Habitacion::costo_total() {
-  return precio_noche * noches;
-}
-
-string Habitacion::to_string() {
-  stringstream aux;
-
-  aux << "Habitacion " << numero
-      << " tipo " << tipo
-      << " noches " << noches
-      << " precio por noche " << precio_noche
-      << " costo total " << costo_total()
-      << "\n";
-
-  return aux.str();
+ostream& operator<<(ostream& salida, const Habitacion& habitacion) {
+  salida << habitacion.to_string();
+  return salida;
 }
