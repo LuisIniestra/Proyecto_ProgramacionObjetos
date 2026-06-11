dc1ed76f50de9bccaa999d4a431a92cc6ab29501
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:10:46 -0600

    Update Suite.cpp

--- Suite.cpp
+++ Suite.cpp
@@ -3,55 +3,59 @@
 
 using namespace std;
 
-Suite::Suite(): Habitacion(0, 0, 0, "suite") {
-  // Constructor por default de Suite
+Suite::Suite(): Habitacion(0, 0, "suite") {
+  // Constructor por default.
 
   servicio_extra = 0;
-
   descuento = 0;
-  // Inicializa las variables en cero
 }
 
-Suite::Suite(int num, int noch, double precio, double servicio):
-  Habitacion(num, noch, precio, "suite") {
-  // Constructor con parámetros. Llama al constructor de Habitacion para inicializar los atributos heredados
+Suite::Suite(int num, double precio, double servicio):
+  Habitacion(num, precio, "suite") {
+  // Constructor con servicio extra.
 
   servicio_extra = servicio;
-  // Guarda el servicio extra recibido
-
   descuento = 0;
-  // Como este constructor no recibe descuento, se deja en cero
 }
 
-Suite::Suite(int num, int noch, double precio, double servicio, double desc):
-  Habitacion(num, noch, precio, "suite") {
-  // Constructor sobrecargado. Recibe número, noches, precio, servicio extra y descuento
-  // Aquí se aplica sobrecarga porque Suite tiene varios constructores con distintos parámetros
+Suite::Suite(int num, double precio, double servicio, double desc):
+  Habitacion(num, precio, "suite") {
+  // Constructor sobrecargado.
+  // Recibe servicio extra y descuento.
 
   servicio_extra = servicio;
-  // Guarda el servicio extra recibido
-
   descuento = desc;
-  // Guarda el descuento recibido
 }
 
 double Suite::costo_total() const {
-  // Implementación de costo_total() de la clase abstracta
-  
+  // Cálculo propio de suite.
+  // Aquí se aplica overriding.
+
+  if (disponible) {
+    return 0;
+  }
+
   return (precio_noche * noches) + servicio_extra - descuento;
-  // Calcula el costo total de la suite
 }
 
 string Suite::to_string() const {
-  // Implementación de to_string() de la clase abstracta
+  // Convierte los datos de la suite a texto.
 
   stringstream aux;
-  // Se usa para construir el texto que se va a mostrar
 
-  aux << "Habitacion " << numero << " tipo " << tipo << " noches " << noches << " precio por noche " << precio_noche << " servicio extra " << servicio_extra << " descuento " << descuento << " costo total " << costo_total() << "\n";
-  // Aquí se arma el texto de salida
-  // Usa atributos heredados de Habitacion y atributos propios de Suite
+  aux << "Habitacion " << numero
+      << " tipo " << tipo
+      << " estado " << (disponible ? "disponible" : "ocupada")
+      << " precio por noche " << precio_noche
+      << " servicio extra " << servicio_extra
+      << " descuento " << descuento;
+
+  if (!disponible) {
+    aux << " noches " << noches
+        << " costo total " << costo_total();
+  }
+
+  aux << "\n";
 
   return aux.str();
-  // Convierte el stringstream en string y lo regresa
 }
