4105fe689be2db292b2a4171d5f9aba98e72f722
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:26:36 -0600

    Update Doble.h

--- Doble.h
+++ Doble.h
@@ -7,40 +7,28 @@
 using namespace std;
 
 class Doble: public Habitacion {
-  // La clase Doble hereda de Habitacion.
-  // Aquí se aplica herencia.
-  // Doble es una clase concreta porque implementará los métodos abstractos de Habitacion.
+  // Doble hereda de Habitacion.
 
   private:
     double cargo_extra;
     // Atributo propio de la habitación doble.
-    // Es private porque solo la clase Doble debe modificarlo directamente.
-    // Aquí se aplica encapsulamiento.
 
   public:
     Doble();
     // Constructor por default.
-    // Permite crear una habitación doble con valores base.
-    // También ayuda a demostrar sobrecarga de constructores.
 
     Doble(int num, int noch, double precio);
-    // Constructor con parámetros básicos.
-    // Recibe número de habitación, noches y precio.
-    // No recibe cargo extra, por eso el cargo se puede dejar en 0.
+    // Constructor con número, noches y precio.
 
     Doble(int num, int noch, double precio, double cargo);
     // Constructor sobrecargado.
-    // Recibe número, noches, precio y cargo extra.
-    // Aquí se aplica overloading porque hay varios constructores Doble con diferentes parámetros.
+    // Permite agregar cargo extra.
 
     double costo_total() const override;
-    // Sobreescritura del método costo_total().
-    // Este método venía como virtual puro en Habitacion.
-    // Doble está obligada a implementarlo porque Habitacion es abstracta.
+    // Sobreescribe el método abstracto costo_total().
 
     string to_string() const override;
-    // Sobreescritura del método to_string().
-    // Sirve para que Doble tenga su propia forma de mostrar información.
+    // Sobreescribe el método abstracto to_string().
 };
 
 #endif
