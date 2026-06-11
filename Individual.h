24929c6c9be83cdcd9461659d541c55e6e0d645a
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:08:26 -0600

    Update Individual.h

--- Individual.h
+++ Individual.h
@@ -7,17 +7,29 @@
 using namespace std;
 
 class Individual: public Habitacion {
+  // Individual hereda de Habitacion.
+  // Aquí se aplica herencia.
 
   private:
     bool desayuno_incluido;
+    // Atributo propio de la habitación individual.
 
   public:
     Individual();
+    // Constructor por default.
+
     Individual(int num, double precio);
+    // Constructor con número y precio base.
+
     Individual(int num, double precio, bool desayuno);
+    // Constructor sobrecargado.
+    // Permite indicar si incluye desayuno.
 
     double costo_total() const override;
+    // Sobreescribe el método abstracto de Habitacion.
+
     string to_string() const override;
+    // Sobreescribe el método abstracto de Habitacion.
 };
 
 #endif
