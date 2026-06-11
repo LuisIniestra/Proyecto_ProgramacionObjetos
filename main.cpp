e2011e132cd2a776637f1db4e9484e787448cfb7
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:21:28 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,117 +5,158 @@
 using namespace std;
 
 int main() {
-  // Inicio del programa.
 
   Hotel hotel;
-  // Crea el objeto principal del sistema.
-
-  hotel.crea_habitaciones_hotel();
-  // Crea las 88 habitaciones fijas del hotel.
 
   int opcion = 0;
-  int noches = 0;
-  int tipo_opcion = 0;
+  int numero;
+  int noches;
+  double precio;
+  double cargo;
+  double servicio;
+  double descuento;
   string tipo;
+  char respuesta;
 
   do {
-    // Menú interactivo para el usuario.
-
     cout << "\n===== SISTEMA DE HOTEL =====\n";
-    cout << "1. Mostrar habitaciones disponibles\n";
-    cout << "2. Reservar habitacion individual\n";
-    cout << "3. Reservar habitacion doble\n";
-    cout << "4. Reservar suite\n";
-    cout << "5. Mostrar habitaciones ocupadas\n";
+    cout << "1. Agregar habitacion individual\n";
+    cout << "2. Agregar habitacion doble\n";
+    cout << "3. Agregar suite\n";
+    cout << "4. Mostrar todas las habitaciones\n";
+    cout << "5. Mostrar habitaciones por tipo\n";
     cout << "6. Calcular ingreso total del hotel\n";
     cout << "7. Calcular ingreso por tipo de habitacion\n";
-    cout << "8. Mostrar todas las habitaciones\n";
-    cout << "9. Salir\n";
+    cout << "8. Salir\n";
     cout << "Selecciona una opcion: ";
     cin >> opcion;
 
     if (opcion == 1) {
-      cout << "\nHabitaciones disponibles:\n\n";
-      hotel.muestra_disponibles();
+
+      cout << "\nNumero de habitacion: ";
+      cin >> numero;
+
+      cout << "Numero de noches: ";
+      cin >> noches;
+
+      cout << "Precio por noche: ";
+      cin >> precio;
+
+      cout << "Incluye desayuno? (s/n): ";
+      cin >> respuesta;
+
+      if (respuesta == 's' || respuesta == 'S') {
+        hotel.agrega_individual(numero, noches, precio, true);
+      }
+      else {
+        hotel.agrega_individual(numero, noches, precio);
+      }
+
+      cout << "Habitacion individual agregada.\n";
     }
 
     else if (opcion == 2) {
-      cout << "\nNumero de noches: ";
+
+      cout << "\nNumero de habitacion: ";
+      cin >> numero;
+
+      cout << "Numero de noches: ";
       cin >> noches;
 
-      hotel.reservar_habitacion("individual", noches);
-      // Reserva automáticamente la primera individual disponible.
+      cout << "Precio por noche: ";
+      cin >> precio;
+
+      cout << "Deseas agregar cargo extra? (s/n): ";
+      cin >> respuesta;
+
+      if (respuesta == 's' || respuesta == 'S') {
+        cout << "Cargo extra: ";
+        cin >> cargo;
+
+        hotel.agrega_doble(numero, noches, precio, cargo);
+      }
+      else {
+        hotel.agrega_doble(numero, noches, precio);
+      }
+
+      cout << "Habitacion doble agregada.\n";
     }
 
     else if (opcion == 3) {
-      cout << "\nNumero de noches: ";
+
+      cout << "\nNumero de habitacion: ";
+      cin >> numero;
+
+      cout << "Numero de noches: ";
       cin >> noches;
 
-      hotel.reservar_habitacion("doble", noches);
-      // Reserva automáticamente la primera doble disponible.
+      cout << "Precio por noche: ";
+      cin >> precio;
+
+      cout << "Servicio extra: ";
+      cin >> servicio;
+
+      cout << "Deseas agregar descuento? (s/n): ";
+      cin >> respuesta;
+
+      if (respuesta == 's' || respuesta == 'S') {
+        cout << "Descuento: ";
+        cin >> descuento;
+
+        hotel.agrega_suite(numero, noches, precio, servicio, descuento);
+      }
+      else {
+        hotel.agrega_suite(numero, noches, precio, servicio);
+      }
+
+      cout << "Suite agregada.\n";
     }
 
     else if (opcion == 4) {
-      cout << "\nNumero de noches: ";
-      cin >> noches;
 
-      hotel.reservar_habitacion("suite", noches);
-      // Reserva automáticamente la primera suite disponible.
-    }
-
-    else if (opcion == 5) {
-      cout << "\nHabitaciones ocupadas:\n\n";
-      hotel.muestra_ocupadas();
-    }
-
-    else if (opcion == 6) {
-      cout << "\nIngreso total del hotel: " << hotel.calc_ingreso_total() << endl;
-    }
-
-    else if (opcion == 7) {
-      cout << "\nTipo de habitacion:\n";
-      cout << "1. Individual\n";
-      cout << "2. Doble\n";
-      cout << "3. Suite\n";
-      cout << "Selecciona una opcion: ";
-      cin >> tipo_opcion;
-
-      if (tipo_opcion == 1) {
-        tipo = "individual";
-      }
-      else if (tipo_opcion == 2) {
-        tipo = "doble";
-      }
-      else if (tipo_opcion == 3) {
-        tipo = "suite";
-      }
-      else {
-        tipo = "";
-      }
-
-      if (tipo != "") {
-        cout << "Ingreso total por " << tipo << ": ";
-        cout << hotel.calc_ingreso_total(tipo) << endl;
-      }
-      else {
-        cout << "Tipo no valido.\n";
-      }
-    }
-
-    else if (opcion == 8) {
-      cout << "\nTodas las habitaciones del hotel:\n\n";
+      cout << "\nHabitaciones registradas:\n\n";
       hotel.muestra_habitaciones();
     }
 
-    else if (opcion == 9) {
+    else if (opcion == 5) {
+
+      cout << "\nEscribe el tipo de habitacion que quieres mostrar.\n";
+      cout << "Opciones: individual, doble, suite\n";
+      cout << "Tipo: ";
+      cin >> tipo;
+
+      cout << "\nHabitaciones tipo " << tipo << ":\n\n";
+      hotel.muestra_habitaciones(tipo);
+    }
+
+    else if (opcion == 6) {
+
+      cout << "\nIngreso total del hotel: ";
+      cout << hotel.calc_ingreso_total() << endl;
+    }
+
+    else if (opcion == 7) {
+
+      cout << "\nEscribe el tipo de habitacion para calcular ingreso.\n";
+      cout << "Opciones: individual, doble, suite\n";
+      cout << "Tipo: ";
+      cin >> tipo;
+
+      cout << "Ingreso total por habitaciones tipo " << tipo << ": ";
+      cout << hotel.calc_ingreso_total(tipo) << endl;
+    }
+
+    else if (opcion == 8) {
+
       cout << "\nSaliendo del sistema...\n";
     }
 
     else {
-      cout << "\nOpcion no valida.\n";
+
+      cout << "\nOpcion no valida. Intenta de nuevo.\n";
     }
 
-  } while (opcion != 9);
+  } while (opcion != 8);
 
   return 0;
 }
