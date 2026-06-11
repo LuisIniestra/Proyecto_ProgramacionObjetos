967847fdf46ebf1e0b3571cf01125bee2f9f1c29
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:10:07 -0600

    Update Doble.cpp

--- Doble.cpp
+++ Doble.cpp
@@ -3,44 +3,55 @@
 
 using namespace std;
 
-Doble::Doble(): Habitacion(0, 0, 0, "doble") {
-  // Constructor por default de Doble
-  
-  cargo_extra = 0;
-  // Inicializa el cargo extra en cero
-}
-
-Doble::Doble(int num, int noch, double precio):
-  Habitacion(num, noch, precio, "doble") {
-  // Constructor con parámetros básicos. Recibe número, noches y precio.
+Doble::Doble(): Habitacion(0, 0, "doble") {
+  // Constructor por default.
 
   cargo_extra = 0;
-
 }
 
-Doble::Doble(int num, int noch, double precio, double cargo):
-  Habitacion(num, noch, precio, "doble") {
-  // Constructor sobrecargado. Recibe número, noches, precio y cargo extra 
+Doble::Doble(int num, double precio):
+  Habitacion(num, precio, "doble") {
+  // Constructor con número y precio.
 
-  cargo_extra = cargo; // Guarda el cargo extra recibido.
+  cargo_extra = 0;
+}
+
+Doble::Doble(int num, double precio, double cargo):
+  Habitacion(num, precio, "doble") {
+  // Constructor sobrecargado.
+  // Recibe un cargo extra.
+
+  cargo_extra = cargo;
 }
 
 double Doble::costo_total() const {
-  // Implementación de costo_total() de la clase abstracta
+  // Cálculo propio de habitación doble.
+  // Aquí se aplica overriding.
+
+  if (disponible) {
+    return 0;
+  }
 
   return (precio_noche * noches) + cargo_extra;
-  // Calcula el costo de la habitación doble.
 }
 
 string Doble::to_string() const {
-  // Implementación de to_string() de la clase abstracta
-  
-  stringstream aux;
-  // Se usa para construir un texto con los datos de la habitación.
+  // Convierte los datos de la habitación doble a texto.
 
-  aux << "Habitacion " << numero << " tipo " << tipo << " noches " << noches << " precio por noche " << precio_noche << " cargo extra " << cargo_extra << " costo total " << costo_total() << "\n";
-  // Usa atributos heredados de Habitacion y el atributo propio cargo_extra.
+  stringstream aux;
+
+  aux << "Habitacion " << numero
+      << " tipo " << tipo
+      << " estado " << (disponible ? "disponible" : "ocupada")
+      << " precio por noche " << precio_noche
+      << " cargo extra " << cargo_extra;
+
+  if (!disponible) {
+    aux << " noches " << noches
+        << " costo total " << costo_total();
+  }
+
+  aux << "\n";
 
   return aux.str();
-  // Convierte el stringstream a string y lo regresa.
 }
