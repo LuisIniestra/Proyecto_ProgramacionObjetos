e9efb9832d5a34aea34135761c0146c7f8aac862
LuisIniestra <luisiniestra.alberto23@gmail.com>
2026-06-11 00:26:02 -0600

    Update Individual.h

--- Individual.h
+++ Individual.h
@@ -7,43 +7,29 @@
 using namespace std;
 
 class Individual: public Habitacion {
-  // La clase Individual hereda de Habitacion.
+  // Individual hereda de Habitacion.
   // Aquí se aplica herencia.
-  // Individual es una habitación concreta, a diferencia de Habitacion que es abstracta.
 
   private:
     bool desayuno_incluido;
     // Atributo propio de la habitación individual.
-    // Es private porque solo la clase Individual debe controlarlo directamente.
-    // Aquí se aplica encapsulamiento.
 
   public:
     Individual();
     // Constructor por default.
-    // Permite crear una habitación individual con valores base.
-    // También ayuda a mostrar sobrecarga de constructores.
 
     Individual(int num, int noch, double precio);
-    // Constructor con parámetros básicos.
-    // Recibe número de habitación, noches y precio.
-    // No recibe desayuno, por eso se puede manejar como false por default.
+    // Constructor con número, noches y precio.
 
     Individual(int num, int noch, double precio, bool desayuno);
     // Constructor sobrecargado.
-    // Recibe los mismos datos anteriores, pero también si incluye desayuno.
-    // Aquí se ve overloading porque hay varios constructores Individual con diferentes parámetros.
+    // Permite indicar si incluye desayuno.
 
     double costo_total() const override;
-    // Sobreescritura del método costo_total().
-    // Este método venía como virtual puro en Habitacion.
-    // Individual está obligada a implementarlo porque Habitacion es abstracta.
-    // Aquí se aplica overriding.
+    // Sobreescribe el método abstracto costo_total() de Habitacion.
 
     string to_string() const override;
-    // Sobreescritura del método to_string().
-    // También venía como virtual puro en Habitacion.
-    // Sirve para que Individual tenga su propia forma de mostrar información.
-    // Aquí también se aplica overriding.
+    // Sobreescribe el método abstracto to_string() de Habitacion.
 };
 
 #endif
